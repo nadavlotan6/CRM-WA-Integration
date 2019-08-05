f21f2689147b610bbc4e4181a0d0966e01a291aa
nadavlotan <nadavlotan6@gmail.com>
2019-08-05 19:35:17 +0300

    first version ready

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = new express();
+const port = process.env.PORT || 3000;
+const server = require('http').createServer(app);
+const io = require('socket.io')(server)
+const dir = __dirname;
+const fs = require('fs');
+
+global.phone = "";
+
+
+app.use(express.static('public'));
+
+app.get('/:phone', (req, res) => {
+    phone = req.params.phone;
+    console.log(phone);
+
+    res.sendFile(dir + '/index.html')
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.emit('phoneNum', phone);
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('test', function (data) {
+        console.log(data);
+    })
+});
+
+
+server.listen(port, () => {
+    console.log("listening on port 3000");
+});
